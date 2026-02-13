1ec9ecc87a1e083102f282532bdf11f00d944f00
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-13 17:42:52 +0100

    commi tt till here

--- lost-found/app/page.tsx
+++ lost-found/app/page.tsx
@@ -3,7 +3,17 @@ import Post from "../app/CreatPost"
 export default function Home() {
   return (
     <div className=""> 
-      <Post></Post>
+      <p> What's he  situation ? </p> 
+      <div>
+        <button>
+          <img src="" alt="" />
+          <p>I lost Something</p>
+        </button>
+        <button>
+          <img src="" alt="" />
+          <p>I found Something</p>
+        </button>
+      </div>
     </div>
   );
 }
