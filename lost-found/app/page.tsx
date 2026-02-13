8b6caa0046a6edbbf62a1ac889a02a644519cfbd
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-13 15:43:44 +0100

    exported and imported the postt page

--- lost-found/app/page.tsx
+++ lost-found/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
+import Post from "../app/CreatPost"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>
-          hello world its me aymen !!
-        </h1>
-        <p>im aymen </p>
-      </main>
+    <div className=""> 
+      <Post></Post>
     </div>
   );
 }
