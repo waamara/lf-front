87a16fbcd6b4d47a5f992c890a52eebdcdac7746
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-17 09:07:19 +0100

    commit

--- lost-found/app/CreatPost.tsx
+++ lost-found/app/CreatPost.tsx
@@ -1,9 +1,9 @@
 
 const CreatPost = () => {
     return (
-        <div className=" bg-blue-200">
-            <p className=" text-2xl font-semibold " > What's he  situation ? </p>
-            <div>
+        <div className="  m-4">
+            <p className=" text-2xl font-semibold m " > What's he  situation ? </p>
+            <div className="flex-row p-4 flex-between items-center ">
                 <button>
                     <p>I lost Something</p>
                 </button>
@@ -11,16 +11,27 @@ const CreatPost = () => {
                     <p>I found Something</p>
                 </button>
             </div>
-            <form action="">
-                <label htmlFor="">Item Title</label>
-                <input type="text" />
-                <label htmlFor="">Description</label>
-                <input type="text" />
-                <label htmlFor="">Last seen Location</label>
-                <input type="text" />
-                <label htmlFor="">Item Title</label>
-                <input type="image" />
-                <input type="submit" />
+            <form action="" className="">
+                <div className=" flex-col  items-center pt-2 ">
+                    <label htmlFor="" className="text-gray-600   ">Item Title</label>
+                    <input type="text" className=" h-6 w-full  bg-amber-100 placeholder:text-gray-500   border-gray-500 border-0.5 rounded-md " />
+                </div>
+                <div className=" flex-col  items-center pt-2 ">
+                    <label htmlFor="" className="text-gray-600   ">Item Description</label>
+                    <input type="text" className=" h-6 w-full  bg-amber-100 placeholder:text-gray-500   border-gray-500 border-0.5 rounded-md " />
+                </div>
+                <div className=" flex-col  items-center pt-2 ">
+                    <label htmlFor="" className="text-gray-600   ">Item Title</label>
+                    <input type="text" className=" h-6 w-full  bg-amber-100 placeholder:text-gray-500   border-gray-500 border-0.5 rounded-md " />
+                </div>
+                <div className=" flex-col  items-center pt-2 ">
+                    <label htmlFor="" className="text-gray-600   ">Item Location</label>
+                    <input type="text" className=" h-6 w-full  bg-amber-100 placeholder:text-gray-500   border-gray-500 border-0.5 rounded-md " />
+                </div>
+                <div>
+                    <button> Post My Item</button>
+                </div>
+
             </form>
             <p>By posting , yu agree o our community guidelines .  Please do not include sensitive personal informaion .</p>
         </div>
