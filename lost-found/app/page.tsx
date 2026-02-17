5822afab734bc1266351d870936b7ca5f54fa28b
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-17 08:35:52 +0100

    commit changes

--- lost-found/app/page.tsx
+++ lost-found/app/page.tsx
@@ -2,30 +2,8 @@ import Post from "../app/CreatPost"
 
 export default function Home() {
   return (
-    <div className="">
-      <p> What's he  situation ? </p>
-      <div>
-        <button>
-          <img src="" alt="" />
-          <p>I lost Something</p>
-        </button>
-        <button>
-          <img src="" alt="" />
-          <p>I found Something</p>
-        </button>
-      </div>
-      <form action="">
-        <label htmlFor="">Item Title</label>
-          <input type="text" />
-        <label htmlFor="">Description</label>
-          <input type="text" />
-        <label htmlFor="">Last seen Location</label>
-          <input type="text" />
-        <label htmlFor="">Item Title</label> 
-          <input type="image" />
-        <input type="submit" />
-      </form>
-      
-    </div>
+<div>
+  <Post />
+</div>
   );
 }
