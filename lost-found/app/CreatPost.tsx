193a3b173e18cfb534fcafa013b4d539c443344e
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-17 08:34:24 +0100

    commmit changes

--- lost-found/app/CreatPost.tsx
+++ lost-found/app/CreatPost.tsx
@@ -1,8 +1,30 @@
 
 const CreatPost = () => {
     return (
-        <div className="bg-white">
-            <p>hello world </p>
+        <div className="">
+            <p> What's he  situation ? </p>
+            <div>
+                <button>
+                    <img src="" alt="" />
+                    <p>I lost Something</p>
+                </button>
+                <button>
+                    <img src="" alt="" />
+                    <p>I found Something</p>
+                </button>
+            </div>
+            <form action="">
+                <label htmlFor="">Item Title</label>
+                <input type="text" />
+                <label htmlFor="">Description</label>
+                <input type="text" />
+                <label htmlFor="">Last seen Location</label>
+                <input type="text" />
+                <label htmlFor="">Item Title</label>
+                <input type="image" />
+                <input type="submit" />
+            </form>
+            <p>By posting , yu agree o our community guidelines .  Please do not include sensitive personal informaion .</p>
         </div>
     )
 }
