6b44b1a6ba6ebfb278636ba96e8002efa3fbb9b8
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-20 12:00:05 +0100

    added the  new pst details tto my pae .tsx

--- lost-found/app/page.tsx
+++ lost-found/app/page.tsx
@@ -1,9 +1,10 @@
 import Post from "../app/CreatPost"
-
+import Postdetails from "../app/Postdetails"
 export default function Home() {
   return (
 <div className="  ">
   <Post />
+  <Postdetails /> 
 </div>
   );
 }
