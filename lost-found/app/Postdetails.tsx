9de9f4cac2baea7f5fe76b9c876afcbae0c0359f
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-02-20 12:10:14 +0100

    commit all chanes till her

--- lost-found/app/Postdetails.tsx
+++ lost-found/app/Postdetails.tsx
@@ -1,11 +1,17 @@
-
+import Image from "next/image";
 
 const Postdetails = () => {
     return (
-        <div className="m-12">
-            <img src="" alt="Image of the lost or found object" className=" rounded-xl p-4  " />
+        <div className="m-4">
+            <Image
+                src="/images/images1.jpg"
+                alt="Image of the lost or found object"
+                width={500}
+                height={500}
+                className=" rounded-xl p"
+            />
         </div>
-    )
-}
+    );
+};
 
-export default Postdetails
+export default Postdetails;
